d66afbdd9675fac9cbd33e23a8be9773ca87bf87
ThomasCornes <61535655+ThomasCornes@users.noreply.github.com>
2021-03-21 11:57:27 +1000

    Add files via upload

---
+++ Prac_03.py
@@ -0,0 +1,20 @@
+# import os
+# print("The files and folders in {} are:".format(os.getcwd()))
+# items = os.listdir('.')
+# for item in items:
+#     print(item)
+# def main():
+#     min_length = 4
+#     max_length = 12
+#     password = input("Please enter password ")
+#     if password < len(min_length):
+#         print("Password is invalid")
+#     else:
+#         print("Password is Valid")
+#     if password > len(max_length):
+#         print("Password is invalid")
+#     else:
+#         print("Password is valid")
+#     for char in len (password):
+#         print("***")
+# main() 

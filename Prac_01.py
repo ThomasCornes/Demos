5d4507f7f5b5ee575a8d81f0a81fbb64a584adcf
ThomasCornes <61535655+ThomasCornes@users.noreply.github.com>
2021-03-21 11:22:13 +1000

    Add files via upload

---
+++ Prac_01.py
@@ -0,0 +1,41 @@
+# """
+# Program to calculate and display a user's bonus based on sales.
+# If sales are under $1,000, the user gets a 10% bonus.
+# If sales are $1,000 or over, the bonus is 15%.
+# """
+# sales = float(input("Enter sales: "))
+# if sales < 0:
+#     print("Invalid sales")
+# else:
+#     if sales < 1000:
+#         print("acceptable sales")
+#     if sales > 1000:
+#         print("Excellent Sales")
+# while sales >= 0:
+#     print(sales)
+#     if 1000 > sales:
+#         bonus = sales * 1.10
+#     else:
+#         bonus = sales * 1.15
+#         print(bonus)
+# else:
+#     print(sales)
+#  loops.py
+# for i in range(20,0,-1):
+#     print(i, end=' ')
+# print()
+# 3. Shop_calculator.py
+# item_number = float(input("How many items are there"))
+# ITEM_PRICE = 0
+# item_price = 0
+# number_of_items = 0
+# discount = 0.90
+# while item_number != number_of_items:
+#     number_of_items = number_of_items + 1
+#     item_price = float(input("How much is the item"))
+#     ITEM_PRICE = item_price + item_price
+#     if item_price <100:
+#         item_price = item_price * discount
+#         print("Your Total cost is $",ITEM_PRICE)
+#     else:
+#         print("your total cost is $",ITEM_PRICE)
